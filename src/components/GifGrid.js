6a849a3715298fa74a0e17436a2211301e551490
Manuel Prado <manuelpra20@gmail.com>
2021-05-11 13:52:27 -0400

    otro commit

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -15,7 +15,7 @@ export const GifGrid = ({category}) => {
             <>
                 <h3 className="animate__animated animate__fadeIn">{category}</h3>
 
-                {loading && <p className="animate__animated animate__flash">loading</p> }
+                {loading && <p className="animate__animated animate__flash">loading...</p> }
 
                 <div className="card-grid">
                         {
